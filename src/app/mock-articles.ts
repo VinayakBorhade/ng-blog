d1f86efdfa97ca327af5844dcf8698ccee72c5b5
VinayakBorhade <vinayakborhade@gmail.com>
2020-12-13 13:01:57 +0530

    adding articles to blog

---
+++ src/app/mock-articles.ts
@@ -0,0 +1,22 @@
+import {Article} from './article';
+
+export const ARTICLES: Article[] = [
+    {
+        id: 1,
+        title: "Pink Case",
+        content: "",
+        description: "First case with Sherlock Holmes. Give it a read :)",
+        key: "my-first-article",
+        date: new Date(),
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png"
+    },
+    {
+        id: 2,
+        title: "The Blind Banker",
+        content: "",
+        description: "Next case with Sherlock Holmes. Give it a read :)",
+        key: "my-second-article",
+        date: new Date(),
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png"
+    }
+];
